b11f5a4f73d7514f32947cdcb41cf46cceaeffdb
ZHANGBO0520 <3060880796@qq.com>
2026-05-26 19:59:14 +0800

    Update footer links for profile site

--- components/footer/link.vue
+++ components/footer/link.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col dark:text-zinc-300 my-5 md:my-0 md:justify-self-center">
-    <p class="text-black dark:text-zinc-300 text-base font-semibold">学习指南</p>
-    <NuxtLink to="https://cn.vuejs.org/" target="_blank" class="hover:underline"> Vue </NuxtLink>
-    <NuxtLink to="https://nuxt.com/" target="_blank" class="hover:underline"> Nuxt </NuxtLink>
-    <NuxtLink to="https://vite.dev/" target="_blank" class="hover:underline"> Vite </NuxtLink>
-    <NuxtLink to="https://react.dev/" class="hover:underline"> React</NuxtLink>
+    <p class="text-black dark:text-zinc-300 text-base font-semibold">Site Sections</p>
+    <NuxtLink to="/academic" class="hover:underline"> Academic Work </NuxtLink>
+    <NuxtLink to="/about" class="hover:underline"> Me </NuxtLink>
+    <NuxtLink to="/blogs" class="hover:underline"> Writing </NuxtLink>
+    <NuxtLink to="/categories" class="hover:underline"> Categories </NuxtLink>
   </div>
 </template>
